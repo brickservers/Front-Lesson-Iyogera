7b6ba813abf4146b3b0922d3de9a64c977ec7701
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-10-02 17:54:03 +0100

    Change from vanilla to Vue

---
+++ Front-Lesson-Iyogera/src/views/Incall.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <LiveLessons v-bind:livelessons="livelessons" />
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import LiveLessons from '../components/LiveLessons';
+import axios from 'axios';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy)
+
+export default {
+  name: 'Incall',
+  components: {
+    LiveLessons
+  },
+  data() {
+    return {
+      livelessons: []
+    }
+  },
+  methods: {
+    
+  created() {
+    axios.get('https://iyogera.dev/iyogera2/api/live_lesson')
+      .then(res => this.livelessons = res.data)
+      .catch(err => console.log(err));
+  }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
--- js/get.js
+++ js/get.js
@@ -1,4 +1,35 @@
-const axios = require('axios');
-axios.get('https://iyogera.dev/iyogera2').then(resp => { 
-	console.log(resp.data); 
-});
+
+export default {
+  name: 'Home',
+  components: {
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+}
