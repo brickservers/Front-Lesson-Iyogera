8ff93e6a1a08f76f88c1c20cc30970ebeaab8238
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-12-29 18:01:44 +0100

    Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,50 @@
-<template>
+<!--<template>-->
 
-</template>
+<!--</template>-->
 
-<script>
-    import axios from 'axios';
-    import Vue from 'vue'
-    import Buefy from 'buefy'
-    import 'buefy/dist/buefy.css'
-    Vue.use(Buefy)
+<!--<script>-->
+<!--    import axios from 'axios';-->
+<!--    import Vue from 'vue'-->
+<!--    import Buefy from 'buefy'-->
+<!--    import 'buefy/dist/buefy.css'-->
+<!--    Vue.use(Buefy)-->
 
-    export default {
-        name: "Login",
-        components: {
+<!--    export default {-->
+<!--        name: "Login",-->
+<!--        components: {-->
 
 
-        },
-        data() {
-            return {
-                //isComponentModalActive: false,
-                formProps: {
-                    email: '',
-                    password: ''
-                }
-            }
-        },
-        methods: {
-            signin() {
-                axios.post('https://demo.iyogera.com/api/login',
-                    {email: this.email, password: this.password},
-                    {headers: {'X-Requested-With': 'XMLHttpRequest'}})
-                    .then(
-                        (response) => {
-                            const token = response.data.access_token;
-                            console.log(token);
-                            // const base64Url = token.split('.')[1];
-                            // const base64 = base64Url.replace('-', '+').replace('_', '/');
-                            // console.log(JSON.parse(window.atob(base64)));
-                            localStorage.setItem('token', token);
-                        }
-                    )
-                    .catch(
-                        (error) => console.log(error)
-                    );
-            }
-        }
-    }
-</script>
+<!--        },-->
+<!--        data() {-->
+<!--            return {-->
+<!--                //isComponentModalActive: false,-->
+<!--                formProps: {-->
+<!--                    email: '',-->
+<!--                    password: ''-->
+<!--                }-->
+<!--            }-->
+<!--        },-->
+<!--        methods: {-->
+<!--            signin() {-->
+<!--                axios.post('https://demo.iyogera.com/api/login',-->
+<!--                    {email: this.email, password: this.password},-->
+<!--                    {headers: {'X-Requested-With': 'XMLHttpRequest'}})-->
+<!--                    .then(-->
+<!--                        (response) => {-->
+<!--                            const token = response.data.access_token;-->
+<!--                            console.log(token);-->
+<!--                            // const base64Url = token.split('.')[1];-->
+<!--                            // const base64 = base64Url.replace('-', '+').replace('_', '/');-->
+<!--                            // console.log(JSON.parse(window.atob(base64)));-->
+<!--                            localStorage.setItem('token', token);-->
+<!--                        }-->
+<!--                    )-->
+<!--                    .catch(-->
+<!--                        (error) => console.log(error)-->
+<!--                    );-->
+<!--            }-->
+<!--        }-->
+<!--    }-->
+<!--</script>-->
 
 
